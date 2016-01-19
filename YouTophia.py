2d159f830004447ad2ebd871800af838f1551162
Ayush Goyal <ayush91195@gmail.com>
2016-01-19 18:29:48 +0530

    removed playlist download
    Was unable to search for video links on playlist page so completely
    removed the playlist download option

--- YouTophia.py
+++ YouTophia.py
@@ -15,34 +15,14 @@ def download_video_via_url(video_url,play_name):  # this ffunction will take vid
     print("The download of %s in " % yt.filename + vid_for+" quality is starting")
     vid_for=vid_for.split(' ')
     video = yt.get(vid_for[0],resolution=vid_for[1])
-    if choice is '2':
-        video.download('./%s'%play_name)
-    else :
-        video.download('')
+    video.download('')
     print("Download Competed")
-def download_playlist_via_url():
-    play_url=input("Enter the playlist url (e.g. : https://www.youtube.com/watch?v=abc?list=abcxyz ) : ")
-    playlistpage_source_code=requests.get(play_url)
-    playlistpage_source_code_text=playlistpage_source_code.text
-    playlist_soup=BeautifulSoup(playlistpage_source_code_text,'html.parser')
-    for c in playlist_soup.findAll('h3',{'class':'playlist-title'}):
-        for a in c.findAll('a',{'class':'yt-uix-sessionlink spf-link '}):
-            playlist_name=a.string
-    create_folder(playlist_name)
-    for a in playlist_soup.findAll('a',{'class':'yt-uix-sessionlink spf-link playlist-video clearfix spf-link '}):
-        url='http://www.youtube.com'+a.get('href')
-        download_video_via_url(url,playlist_name)
-
-
 print("Please select from one of the options below :")
 print("1. If you want to download a particular video via its URL")
-print ("2. If you want to download a complete particular playlist")
-print("3. If you want to search YouTube and save results with relevant information in a spreadsheet and text files")
+print("2. If you want to search YouTube and save results with relevant information in a spreadsheet and text files")
 choice = input()
 if choice is '1' :
     url=input("Enter the video url (e.g. : https://www.youtube.com/watch?v=abcxyz  )  : ")
     download_video_via_url(url,'default')
 elif choice is '2':
-    download_playlist_via_url()
-elif choice is '3':
     import YouTube_Search
